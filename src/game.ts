34dbbc78b8fee87be9c37c62bdc1fd8b7307915f
Vicky <vickythewebdev@gmail.com>
2025-11-15 23:52:03 -0600

    [Refactor] Add setGame function

--- src/game.ts
+++ src/game.ts
@@ -1,22 +1,7 @@
 import { Game } from '@core/Game';
 
-const GRAPHICS_WIDTH = window.innerWidth;
-const GRAPHICS_HEIGHT = window.innerHeight;
+export let game: Game;
 
-const LOOP_FRAMES_PER_SECOND = 60;
-
-const SCENE_MANAGER_DEFAULT_SCENE_NAME = 'DeepSeaSplash';
-
-// Set up game.
-export const game = new Game({
-  graphics: {
-    width: GRAPHICS_WIDTH,
-    height: GRAPHICS_HEIGHT,
-  },
-  loop: {
-    framesPerSecond: LOOP_FRAMES_PER_SECOND,
-  },
-  sceneManager: {
-    defaultSceneName: SCENE_MANAGER_DEFAULT_SCENE_NAME,
-  },
-});
+export function setGame(_game: Game) {
+  game = _game;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
-import { game } from './game';
+import { Game } from '@core/Game';
+import { setGame } from './game';
 
 const CANVAS_WIDTH = window.innerWidth;
 const CANVAS_HEIGHT = window.innerHeight;
+
+const gameConfiguration = {
+  graphics: {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
+  loop: {
+    framesPerSecond: 60,
+  },
+  sceneManager: {
+    defaultSceneName: 'DeepSeaSplash',
+  },
+};
 
 function onLoad() {
   // Set up DOM.
   const gameWindow = document.createElement('div');
   const canvas = document.createElement('canvas');
+  const game = new Game(gameConfiguration);
 
   gameWindow.id = 'game-window';
   gameWindow.appendChild(canvas);
@@ -15,6 +30,8 @@ function onLoad() {
   canvas.height = CANVAS_HEIGHT;
 
   document.body.appendChild(gameWindow);
+
+  setGame(game);
 
   // Start game.
   game.initialize(canvas);
