7a22a1b1db6526fb7976fe8470adc57dbc9b3d7b
Vicky <vickythewebdev@gmail.com>
2025-11-16 00:12:05 -0600

    [Feature] Add generateId util

--- src/core/entities/Node.ts
+++ src/core/entities/Node.ts
@@ -1,9 +1,11 @@
+import { generateId } from '@core/utils/generateId';
+
 export abstract class Node<T extends object = object> {
   public readonly id: string;
   public state: T;
 
   constructor() {
-    this.id = ''; // TODO: generate and assign id.
+    this.id = generateId();
     this.state = this.getDefaultState();
   }
 
