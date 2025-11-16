c30adea32312fa470151f9037606e06fa80634fa
Vicky <vickythewebdev@gmail.com>
2025-11-16 02:28:22 -0600

    [Feature] Add unit test coverage for logger

---
+++ src/core/utils/logger.test.ts
@@ -0,0 +1,57 @@
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import { timestampNow } from './dateTimeProvider';
+import { downloadFile } from './downloadFile';
+import { clearLogs, downloadLogs, log } from './logger';
+
+vi.mock('./dateTimeProvider', () => ({
+  timestampNow: vi.fn(),
+}));
+
+vi.mock('./downloadFile', () => ({
+  downloadFile: vi.fn(),
+}));
+
+describe('logger', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+
+    clearLogs();
+  });
+
+  it('should log an error in console', () => {
+    const consoleSpy = vi.spyOn(console, 'error');
+
+    log('Error', 'message');
+
+    expect(consoleSpy).toHaveBeenCalledWith('[Error] message');
+  });
+
+  it('should log a warning in console', () => {
+    const consoleSpy = vi.spyOn(console, 'warn');
+
+    log('Warn', 'message');
+
+    expect(consoleSpy).toHaveBeenCalledWith('[Warn] message');
+  });
+
+  it('should log a message in console', () => {
+    const consoleSpy = vi.spyOn(console, 'log');
+
+    log('Tag', 'message');
+
+    expect(consoleSpy).toHaveBeenCalledWith('[Tag] message');
+  });
+
+  it('should download logs', () => {
+    vi.mocked(timestampNow).mockReturnValueOnce(1000);
+
+    log('1', '2');
+    log('3', '4');
+
+    downloadLogs();
+
+    const blob = new Blob(['[1] 2\n[3] 4'], { type: 'text/plain' });
+
+    expect(downloadFile).toHaveBeenCalledWith('deep_sea_logs-1000.txt', blob);
+  });
+});
