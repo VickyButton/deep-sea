b848e20baffb3e9b7982ca1a342622159c5442af
Vicky <vickythewebdev@gmail.com>
2025-11-18 18:41:39 -0600

    [Feature] Add bubbles to DeepSeaSplash

--- src/scenes/DeepSeaSplash/DeepSeaSplash.ts
+++ src/scenes/DeepSeaSplash/DeepSeaSplash.ts
@@ -3,6 +3,7 @@ import { Scene } from '@core/entities/Scene';
 import { log } from '@core/utils/logger';
 import FpsCounter from '@nodes/FpsCounter/FpsCounter';
 import { game } from 'game';
+import { DeepSeaBubble } from './DeepSeaBubble';
 
 const DEEP_SEA_SPLASH_LOG_TAG = 'Scene/DeepSeaSplash';
 
@@ -10,75 +11,158 @@ interface DeepSeaSplashState extends SceneState {
   elapsed: number;
 }
 
+const MAX_BUBBLE_COUNT = 50;
 const FADE_IN_DURATION = 5000;
-const TITLE_TEXT = 'DeepSea';
+const TITLE_FONT_SIZE = 100;
+const TITLE_FONT_FAMILY = 'Arial';
+const TITLE_TEXT = 'DEEP SEA';
 
 export default class DeepSeaSplash extends Scene<DeepSeaSplashState> {
   private readonly fpsCounter = new FpsCounter();
+  private readonly bubbles: DeepSeaBubble[] = [];
 
   public initialize(): void {
     log(DEEP_SEA_SPLASH_LOG_TAG, 'Initializing...');
+
+    game.camera.setPosition({
+      x: game.camera.width / 2,
+      y: game.camera.height / 2,
+    });
   }
 
   public update(dt: number): void {
     if (this.state.elapsed < FADE_IN_DURATION) this.state.elapsed += dt;
 
     this.fpsCounter.update(dt);
+    this.updateBubbles(dt);
     this.render();
   }
 
+  private updateBubbles(dt: number) {
+    const shouldSpawnBubble = this.bubbles.length < MAX_BUBBLE_COUNT && Math.random() <= 0.03;
+
+    if (shouldSpawnBubble) this.spawnBubble();
+
+    for (let i = 0; i < this.bubbles.length; i++) {
+      const bubble = this.bubbles[i];
+
+      bubble.update(dt);
+
+      if (game.camera.isOffScreen(bubble) || bubble.state.width <= 1) this.bubbles.splice(i, 1);
+    }
+  }
+
+  private spawnBubble() {
+    const diameter = Math.floor(240 * Math.random()) + 1;
+    const x = game.camera.width * Math.random();
+    const y = game.camera.height + diameter / 2;
+    const bubble = new DeepSeaBubble();
+
+    bubble.state.width = diameter;
+    bubble.state.height = diameter;
+    bubble.state.position.x = x;
+    bubble.state.position.y = y;
+
+    bubble.initialize();
+
+    this.bubbles.push(bubble);
+  }
+
   private render() {
+    this.renderBackground();
+    this.renderTitle();
+    this.renderBubbles();
+
+    // If still fading in, render mask.
+    if (this.state.elapsed < FADE_IN_DURATION) this.renderMask();
+  }
+
+  private renderBackground() {
+    const x = game.graphics.width / 2;
+    const y = game.graphics.height / 2;
     const width = game.graphics.width;
     const height = game.graphics.height;
-    const backgroundCanvas = new OffscreenCanvas(width, height);
-    const backgroundContext = backgroundCanvas.getContext('2d');
+    const canvas = new OffscreenCanvas(width, height);
+    const context = canvas.getContext('2d');
 
-    if (!backgroundContext) return;
+    if (!context) throw new Error('Could not get rendering context');
 
-    // Step 1: Render background gradient.
-    const backgroundGradient = backgroundContext.createLinearGradient(
-      width / 2,
-      0,
-      width / 2,
-      height,
-    );
+    const x0 = width / 2;
+    const y0 = height / -2;
+    const r0 = 10;
+    const x1 = x0;
+    const y1 = y0;
+    const r1 = width;
+    const gradient = context.createRadialGradient(x0, y0, r0, x1, y1, r1);
 
-    backgroundGradient.addColorStop(0, '#1b81a7ff');
-    backgroundGradient.addColorStop(1, '#03202bff');
+    gradient.addColorStop(0, '#0f607eff');
+    gradient.addColorStop(0.2, '#0b495f');
+    gradient.addColorStop(1, '#000000');
 
-    backgroundContext.fillStyle = backgroundGradient;
-    backgroundContext.fillRect(0, 0, width, height);
+    context.fillStyle = gradient;
+    context.fillRect(0, 0, width, height);
 
-    // Step 2: Render title text.
-    backgroundContext.font = '100px Arial';
-    backgroundContext.textAlign = 'center';
+    const imageBitmap = canvas.transferToImageBitmap();
 
-    backgroundContext.fillStyle = 'black';
-    backgroundContext.fillText(TITLE_TEXT, width / 2, height / 2 + 8);
-
-    backgroundContext.fillStyle = 'white';
-    backgroundContext.fillText(TITLE_TEXT, width / 2, height / 2);
-
-    // Step 3: If still fading in, render mask.
-    if (this.state.elapsed < FADE_IN_DURATION) {
-      const fadeInMaskAlphaValue = Math.max(0, 1 - this.state.elapsed / FADE_IN_DURATION);
-      const fadeInMaskColor = `rgba(0,0,0,${String(fadeInMaskAlphaValue)})`;
-
-      backgroundContext.fillStyle = fadeInMaskColor;
-      backgroundContext.fillRect(0, 0, width, height);
-    }
-
-    // Convert to ImageBitmap.
-    const imageBitmap = backgroundCanvas.transferToImageBitmap();
-
-    // Add to draw queue.
     game.graphics.addToDrawQueue({
       imageBitmap,
-      position: {
-        x: width / 2,
-        y: height / 2,
-      },
+      position: { x, y },
       layer: 0,
+    });
+  }
+
+  private renderBubbles() {
+    for (const bubble of this.bubbles) bubble.render();
+  }
+
+  private renderMask() {
+    const x = game.graphics.width / 2;
+    const y = game.graphics.height / 2;
+    const width = game.graphics.width;
+    const height = game.graphics.height;
+    const canvas = new OffscreenCanvas(width, height);
+    const context = canvas.getContext('2d');
+
+    if (!context) throw new Error('Could not get rendering context');
+
+    const alphaValue = Math.max(0, 1 - this.state.elapsed / FADE_IN_DURATION);
+    const fillStyle = `rgba(0,0,0,${String(alphaValue)})`;
+
+    context.fillStyle = fillStyle;
+    context.fillRect(0, 0, width, height);
+
+    const imageBitmap = canvas.transferToImageBitmap();
+
+    game.graphics.addToDrawQueue({
+      imageBitmap,
+      position: { x, y },
+      layer: 3,
+    });
+  }
+
+  private renderTitle() {
+    const x = game.graphics.width / 2;
+    const y = game.graphics.height / 2;
+    const width = TITLE_FONT_SIZE * 6;
+    const height = TITLE_FONT_SIZE;
+    const canvas = new OffscreenCanvas(width, height);
+    const context = canvas.getContext('2d');
+
+    if (!context) throw new Error('Could not get rendering context');
+
+    context.font = `${String(TITLE_FONT_SIZE)}px ${TITLE_FONT_FAMILY}`;
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+
+    context.strokeStyle = 'white';
+    context.strokeText(TITLE_TEXT, width / 2, height / 2);
+
+    const imageBitmap = canvas.transferToImageBitmap();
+
+    game.graphics.addToDrawQueue({
+      imageBitmap,
+      position: { x, y },
+      layer: 2,
     });
   }
 
