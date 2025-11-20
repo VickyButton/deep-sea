c921d43fd1ad94646f835f2603cf9ca2ac461145
Vicky <vickythewebdev@gmail.com>
2025-11-20 00:20:37 -0600

    [Feature] Add assets path alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   resolve: {
     alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
       '@core': path.resolve(__dirname, './src/core'),
       '@nodes': path.resolve(__dirname, './src/nodes'),
       '@scenes': path.resolve(__dirname, './src/scenes'),
