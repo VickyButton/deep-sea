4eedef424934fc02499793f25d5a7d20ea040b89
Vicky <vickythewebdev@gmail.com>
2025-11-25 23:54:44 -0600

    [Refactor] Update Scene to fit new node pattern

--- src/core/engine/SceneManager.test.ts
+++ src/core/engine/SceneManager.test.ts
@@ -2,53 +2,50 @@ import { Scene } from '@core/nodes/Scene';
 import { describe, expect, it, vi } from 'vitest';
 import { SceneManager } from './SceneManager';
 
-class TestScene extends Scene {
-  setup = vi.fn();
-  update = vi.fn();
-  teardown = vi.fn();
-}
-
 describe('SceneManager', () => {
   it('should setup new active scene', () => {
     const sceneManager = new SceneManager();
-    const scene = new TestScene();
+    const scene = new Scene();
+    const sceneSetupSpy = vi.spyOn(scene, 'setup');
 
-    expect(scene.setup).not.toHaveBeenCalled();
-
-    sceneManager.setActiveScene(scene);
-
-    expect(scene.setup).toHaveBeenCalledOnce();
-  });
-
-  it('should teardown current active scene when setting new active scene', () => {
-    const sceneManager = new SceneManager();
-    const scene = new TestScene();
+    expect(sceneSetupSpy).not.toHaveBeenCalled();
 
     sceneManager.setActiveScene(scene);
 
-    expect(scene.teardown).not.toHaveBeenCalled();
-
-    sceneManager.setActiveScene(scene);
-
-    expect(scene.teardown).toHaveBeenCalledOnce();
+    expect(sceneSetupSpy).toHaveBeenCalledOnce();
   });
 
   it('should update active scene', () => {
     const sceneManager = new SceneManager();
-    const scene = new TestScene();
+    const scene = new Scene();
+    const sceneUpdateSpy = vi.spyOn(scene, 'update');
 
     sceneManager.setActiveScene(scene);
     sceneManager.updateActiveScene(1);
 
-    expect(scene.update).toHaveBeenCalledWith(1);
+    expect(sceneUpdateSpy).toHaveBeenCalledWith(1);
+  });
+
+  it('should teardown current active scene when setting new active scene', () => {
+    const sceneManager = new SceneManager();
+    const scene = new Scene();
+    const sceneTeardownSpy = vi.spyOn(scene, 'teardown');
+
+    sceneManager.setActiveScene(scene);
+
+    expect(sceneTeardownSpy).not.toHaveBeenCalled();
+
+    sceneManager.setActiveScene(scene);
+
+    expect(sceneTeardownSpy).toHaveBeenCalledOnce();
   });
 
   it('should load scene', async () => {
-    vi.mock('/src/scenes/TestScene/TestScene', () => ({ default: TestScene }));
+    vi.mock('/src/scenes/Scene/Scene', () => ({ default: Scene }));
 
     const sceneManager = new SceneManager();
-    const scene = await sceneManager.loadScene('TestScene');
+    const scene = await sceneManager.loadScene('Scene');
 
-    expect(scene instanceof TestScene).toBe(true);
+    expect(scene instanceof Scene).toBe(true);
   });
 });
--- src/core/nodes/Scene.ts
+++ src/core/nodes/Scene.ts
@@ -1,15 +1,5 @@
 import { GameNode } from '@core/entities/GameNode';
 
-export interface SceneState {
-  name: string;
-}
-
-export abstract class Scene extends GameNode {
-  public state: SceneState = this.getDefaultState();
-
-  private getDefaultState() {
-    return {
-      name: '',
-    };
-  }
+export class Scene extends GameNode {
+  public name = '';
 }
