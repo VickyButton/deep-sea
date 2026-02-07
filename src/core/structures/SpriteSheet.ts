5940143a94cb1688bed530abbf8d185dff9847bd
Vicky <vickythewebdev@gmail.com>
2026-02-06 22:18:06 -0600

    [Refactor] Use Rectangle in SpriteSheet instead of SpriteSheetQuad

--- src/core/structures/SpriteSheet.ts
+++ src/core/structures/SpriteSheet.ts
@@ -1,13 +1,6 @@
 import { getCanvasContext2D } from '@core/utils/getCanvasContext';
 import { Rectangle } from './Rectangle';
 
-interface SpriteSheetQuad {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-}
-
 export class SpriteSheet {
   private readonly image: ImageBitmap;
 
@@ -15,32 +8,31 @@ export class SpriteSheet {
     this.image = image;
   }
 
-  public parseSprite(quad: SpriteSheetQuad) {
-    if (!this.isValidQuad(quad)) throw new Error('Invalid sprite sheet quad');
+  public parseSprite(rectangle: Rectangle) {
+    if (!this.isValidRectangle(rectangle)) throw new Error('Invalid sprite sheet rectangle');
 
-    const canvas = new OffscreenCanvas(quad.width, quad.height);
+    const canvas = new OffscreenCanvas(rectangle.width, rectangle.height);
     const ctx = getCanvasContext2D(canvas);
     const image = this.image;
-    const sx = quad.x;
-    const sy = quad.y;
-    const sw = quad.width;
-    const sh = quad.height;
+    const sx = rectangle.x;
+    const sy = rectangle.y;
+    const sw = rectangle.width;
+    const sh = rectangle.height;
     const dx = 0;
     const dy = 0;
-    const dw = quad.width;
-    const dh = quad.height;
+    const dw = rectangle.width;
+    const dh = rectangle.height;
 
     ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
 
     return canvas.transferToImageBitmap();
   }
 
-  private isValidQuad(quad: SpriteSheetQuad) {
+  private isValidRectangle(rectangle: Rectangle) {
     const imageRectangle = new Rectangle(0, 0, this.image.width, this.image.height);
-    const quadRectangle = new Rectangle(quad.x, quad.y, quad.width, quad.height);
 
-    // TODO: Create Rectangle.containsRectangle method and use here. An overlapping quad does not
-    // equate to a valid quad here as it could potentially lie outside the image.
-    return imageRectangle.overlaps(quadRectangle);
+    // TODO: Create Rectangle.containsRectangle method and use here. An overlapping rectangle does
+    // not equate to a valid rectangle here as it could potentially lie outside the image.
+    return imageRectangle.overlaps(rectangle);
   }
 }
