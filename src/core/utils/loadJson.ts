97b0fc8eb66faec57d6be5d537ace68eee0a76a9
Vicky <vickythewebdev@gmail.com>
2026-02-12 12:26:53 -0600

    [Feature] Add loadJson utility function

---
+++ src/core/utils/loadJson.ts
@@ -0,0 +1,21 @@
+const ERROR_NO_DEFAULT_ON_IMPORT = 'No default export defined';
+
+interface ExportWithDefault {
+  default: unknown;
+}
+
+function hasDefaultExport(value: unknown): value is ExportWithDefault {
+  return typeof value === 'object' && value !== null && 'default' in value;
+}
+
+export async function loadJson(path: string) {
+  const response: unknown = await import(path, {
+    with: {
+      type: 'json',
+    },
+  });
+
+  if (!hasDefaultExport(response)) throw new Error(ERROR_NO_DEFAULT_ON_IMPORT);
+
+  return response.default;
+}
