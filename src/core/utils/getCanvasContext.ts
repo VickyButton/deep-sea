254173bca5a263309a28fa5c9177e804e681e8c9
Vicky <vickythewebdev@gmail.com>
2026-02-13 00:47:56 -0600

    [Feature] Add overloads to getCanvasContext2D

--- src/core/utils/getCanvasContext.ts
+++ src/core/utils/getCanvasContext.ts
@@ -3,7 +3,9 @@
  * @param canvas The canvas to retrieve the context from.
  * @returns The canvas context.
  */
-export function getCanvasContext2D(canvas: OffscreenCanvas) {
+export function getCanvasContext2D(canvas: HTMLCanvasElement): CanvasRenderingContext2D;
+export function getCanvasContext2D(canvas: OffscreenCanvas): OffscreenCanvasRenderingContext2D;
+export function getCanvasContext2D(canvas: HTMLCanvasElement | OffscreenCanvas) {
   const ctx = canvas.getContext('2d');
 
   if (!ctx) throw new Error('Unable to get canvas context');
