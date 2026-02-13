c1151722c55fdbfff9de03f438e2b02651b405ce
Vicky <vickythewebdev@gmail.com>
2026-02-13 02:20:10 -0600

    [Feature] Automatically set up camera in scene setup

--- src/core/nodes/Scene.ts
+++ src/core/nodes/Scene.ts
@@ -1,5 +1,21 @@
+import { getRenderer } from '@core/engine/renderer';
+import { CameraNode2D } from './CameraNode2D';
 import { GameNode } from './GameNode';
 
 export abstract class Scene extends GameNode {
   public abstract readonly title: string;
+  public camera = new CameraNode2D();
+
+  public setup(): void {
+    super.setup();
+
+    this.setupCamera();
+  }
+
+  private setupCamera() {
+    const renderer = getRenderer();
+
+    renderer.setActiveCamera(this.camera);
+    this.addChild(this.camera);
+  }
 }
