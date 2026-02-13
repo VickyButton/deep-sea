594806a908072e87c0a0b8b3fb8091f7687f4cbc
Vicky <vickythewebdev@gmail.com>
2026-02-13 14:52:31 -0600

    [Refactor] Use loop hook in Game

--- src/core/Game.test.ts
+++ src/core/Game.test.ts
@@ -66,16 +66,6 @@ vi.mock('@core/engine/InputController', () => ({
   ),
 }));
 
-vi.mock('@core/engine/Loop', () => ({
-  Loop: vi.fn(
-    class {
-      setLoopCallback = vi.fn();
-      start = vi.fn();
-      stop = vi.fn();
-    },
-  ),
-}));
-
 vi.mock('@core/engine/Physics2D', () => ({
   Physics2D: vi.fn(),
 }));
@@ -107,34 +97,14 @@ describe('Game', () => {
 
   it('should setup components and initial scene on setup', () => {
     const game = new Game();
-    const loopSetLoopCallbackSpy = vi.spyOn(game.loop, 'setLoopCallback');
     const taskManagerRegisterTaskSpy = vi.spyOn(game.taskManager, 'registerTask');
     const sceneManagerLoadSceneSpy = vi.spyOn(game.sceneManager, 'loadScene');
     const sceneManagerSetActiveSceneSpy = vi.spyOn(game.sceneManager, 'setActiveScene');
 
     game.setup();
 
-    expect(loopSetLoopCallbackSpy).toHaveBeenCalled();
     expect(taskManagerRegisterTaskSpy).toHaveBeenCalled();
     expect(sceneManagerLoadSceneSpy).toHaveBeenCalledWith(config.game.splashScene);
     expect(sceneManagerSetActiveSceneSpy).toHaveBeenCalled();
-  });
-
-  it('should start loop on start', () => {
-    const game = new Game();
-    const loopStartSpy = vi.spyOn(game.loop, 'start');
-
-    game.start();
-
-    expect(loopStartSpy).toHaveBeenCalled();
-  });
-
-  it('should stop loop on stop', () => {
-    const game = new Game();
-    const loopStopSpy = vi.spyOn(game.loop, 'stop');
-
-    game.stop();
-
-    expect(loopStopSpy).toHaveBeenCalled();
   });
 });
