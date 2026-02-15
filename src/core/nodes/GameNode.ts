e5e297c780a4ad30011ecb1c0ca7a6568300044b
Vicky <vickythewebdev@gmail.com>
2026-02-15 12:50:12 -0600

    [Refactor] Move Script class into GameNode

--- src/core/nodes/GameNode.ts
+++ src/core/nodes/GameNode.ts
@@ -1,5 +1,23 @@
 import { generateId } from '@core/utils/generateId';
 
+export abstract class Script<T extends GameNode> {
+  /**
+   * Callback to be executed when the node is set up.
+   * @param node The node the script is attached to.
+   */
+  public abstract onSetup(node: T): void;
+  /**
+   * Callback to be executed when the node is updated.
+   * @param node The node the script is attached to.
+   */
+  public abstract onUpdate(node: T): void;
+  /**
+   * Callback to be executed when the node is torn down.
+   * @param node The node the script is attached to.
+   */
+  public abstract onTeardown(node: T): void;
+}
+
 export class GameNode {
   /**
    * The node's unique identifer.
