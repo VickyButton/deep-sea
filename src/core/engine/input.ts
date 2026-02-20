3d05b1c7c26c38018a8c360cdfa31355f59bd0df
Vicky <vickythewebdev@gmail.com>
2026-02-19 23:47:00 -0600

    [Feature] Add keyup and keydown event listeners to input

--- src/core/engine/input.ts
+++ src/core/engine/input.ts
@@ -2,9 +2,19 @@ type KeyboardEventListener = (e: KeyboardEvent) => void;
 
 class InputController {
   private keypressEventListeners = new Set<KeyboardEventListener>();
+  private keydownEventListeners = new Set<KeyboardEventListener>();
+  private keyupEventListeners = new Set<KeyboardEventListener>();
 
   private onKeypress = (e: KeyboardEvent) => {
     for (const eventListener of this.keypressEventListeners) eventListener(e);
+  };
+
+  private onKeydown = (e: KeyboardEvent) => {
+    for (const eventListener of this.keydownEventListeners) eventListener(e);
+  };
+
+  private onKeyup = (e: KeyboardEvent) => {
+    for (const eventListener of this.keyupEventListeners) eventListener(e);
   };
 
   /**
@@ -26,10 +36,48 @@ class InputController {
   }
 
   /**
+   * Adds an event listener for the 'keydown' event.
+   *
+   * @param eventListener The event listener to add.
+   */
+  public addKeydownEventListener(eventListener: KeyboardEventListener) {
+    this.keydownEventListeners.add(eventListener);
+  }
+
+  /**
+   * Removes an event listener for the 'keydown' event.
+   *
+   * @param eventListener The event listener to remove.
+   */
+  public removeKeydownEventListener(eventListener: KeyboardEventListener) {
+    this.keydownEventListeners.delete(eventListener);
+  }
+
+  /**
+   * Adds an event listener for the 'keyup' event.
+   *
+   * @param eventListener The event listener to add.
+   */
+  public addKeyupEventListener(eventListener: KeyboardEventListener) {
+    this.keyupEventListeners.add(eventListener);
+  }
+
+  /**
+   * Removes an event listener for the 'keyup' event.
+   *
+   * @param eventListener The event listener to remove.
+   */
+  public removeKeyupEventListener(eventListener: KeyboardEventListener) {
+    this.keyupEventListeners.delete(eventListener);
+  }
+
+  /**
    * Attaches event delegation listeners from the document.
    */
   public attachListeners() {
     document.addEventListener('keypress', this.onKeypress);
+    document.addEventListener('keydown', this.onKeydown);
+    document.addEventListener('keyup', this.onKeyup);
   }
 
   /**
@@ -37,6 +85,8 @@ class InputController {
    */
   public detachListeners() {
     document.removeEventListener('keypress', this.onKeypress);
+    document.removeEventListener('keydown', this.onKeydown);
+    document.removeEventListener('keyup', this.onKeyup);
   }
 }
 
