5aafd9de6011f71a6186f0a432b7eeb45262d192
vickybutton <vickythewebdev@gmail.com>
2026-09-02 18:49:45 -0500

    feature: Scaffold Main class

---
+++ src/Main.ts
@@ -0,0 +1,15 @@
+export class Main {
+  /**
+   * Starts the main instance.
+   */
+  public start() {
+    // TODO: Implement.
+  }
+
+  /**
+   * Stops the main instance.
+   */
+  public stop() {
+    // TODO: Implement.
+  }
+}
