ef6826336fab3f09860e82c47b9fe85092faeb1d
vickybutton <vickythewebdev@gmail.com>
2026-09-07 00:51:19 -0500

    chore: Rename CanvasItem to CanvasNode

--- src/restructure/nodes/CanvasNode.ts
+++ src/restructure/nodes/CanvasNode.ts
@@ -4,7 +4,7 @@ import { BaseNode } from './BaseNode';
 /**
  * Abstract base node for nodes which can be drawn onto a canvas.
  */
-export abstract class CanvasItem extends BaseNode {
+export abstract class CanvasNode extends BaseNode {
   /** A flag indicating if the node may be drawn or not. */
   public isVisible = true;
   /** The order in which this node is drawn. Nodes with higher z-indices are drawn on top of nodes with lower z-indices. */
