6eef3d8b8988d631ddc2941de8931d687a81a4ea
vickybutton <vickythewebdev@gmail.com>
2026-09-08 15:08:24 -0500

    feature: Export nodes

---
+++ src/restructure/nodes/index.ts
@@ -0,0 +1,8 @@
+/**
+ * This file defines node exports for the package.
+ */
+import { BaseNode } from './BaseNode';
+import { CanvasNode } from './CanvasNode';
+import { Node2D } from './Node2D';
+
+export { BaseNode, CanvasNode, Node2D };
