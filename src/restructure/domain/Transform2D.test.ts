558902ba6f0438be433274fe6da949ee2df1d179
vickybutton <vickythewebdev@gmail.com>
2026-09-09 02:16:37 -0500

    feature: Add functionality for applying transform matrix to a vector

---
+++ src/restructure/domain/Transform2D.test.ts
@@ -0,0 +1,76 @@
+import { Transform2D } from './Transform2D';
+import { Vector2D } from './Vector2D';
+import { describe, expect, it } from 'vitest';
+
+// Cosine(90 degrees) does not return 0, but rather a nuber close to zero.
+const ZERO_APPROXIMATION = 6.123233995736766e-17;
+
+describe('Transform2D', () => {
+  it('applies translation to vector', () => {
+    const transform = new Transform2D({
+      position: [1, 1],
+    });
+    const vector = new Vector2D(1, 1);
+    const result = transform.applyTransform(vector);
+
+    expect(result).toEqual({
+      x: 2,
+      y: 2,
+    });
+  });
+
+  it('applies rotation to vector', () => {
+    const transform = new Transform2D({
+      rotation: Math.PI / 2, // 90 degrees
+    });
+    const vector = new Vector2D(1, 1);
+    const result = transform.applyTransform(vector);
+
+    expect(result).toEqual({
+      x: -1 + ZERO_APPROXIMATION,
+      y: 1,
+    });
+  });
+
+  it('applies scaling to vector', () => {
+    const transform = new Transform2D({
+      scale: [2, 2],
+    });
+    const vector = new Vector2D(1, 1);
+    const result = transform.applyTransform(vector);
+
+    expect(result).toEqual({
+      x: 2,
+      y: 2,
+    });
+  });
+
+  it('applies translation and rotation to vector', () => {
+    const transform = new Transform2D({
+      position: [1, 1],
+      rotation: Math.PI / 2, // 90 degrees
+    });
+    const vector = new Vector2D(1, 1);
+    const result = transform.applyTransform(vector);
+
+    expect(result).toEqual({
+      x: 2 * (-1 + ZERO_APPROXIMATION),
+      y: 2,
+    });
+  });
+
+  it('applies translation, rotation, and scaling to vector', () => {
+    const transform = new Transform2D({
+      position: [1, 1],
+      rotation: Math.PI / 2, // 90 degrees
+      scale: [2, 2],
+    });
+    const vector = new Vector2D(1, 1);
+    const result = transform.applyTransform(vector);
+
+    expect(result).toEqual({
+      x: 4 * (-1 + ZERO_APPROXIMATION),
+      y: 4,
+    });
+  });
+});
--- src/restructure/domain/Transform2D.ts
+++ src/restructure/domain/Transform2D.ts
@@ -4,9 +4,9 @@ import { Vector2D } from './Vector2D';
  * A 2D transform matrix.
  */
 export class Transform2D {
-  /** The transform position matrix. */
+  /** The transform position vector. */
   public position: Vector2D;
-  /** The transform scale matrix. */
+  /** The transform scalar vector. */
   public scale: Vector2D;
   /** The transform rotation in radians. */
   public rotation: number;
@@ -15,6 +15,56 @@ export class Transform2D {
     this.position = options?.position ? new Vector2D(options.position[0], options.position[1]) : new Vector2D();
     this.scale = options?.scale ? new Vector2D(options.scale[0], options.scale[1]) : new Vector2D(1, 1);
     this.rotation = options?.rotation ?? 0;
+  }
+
+  /**
+   * Applies the transformation matrix to a vector.
+   * @param vector The vector to apply the transformation matrix to.
+   * @returns The resulting vector after being transformed.
+   */
+  public applyTransform(vector: Vector2D) {
+    let result = vector;
+    result = this.applyTranslation(result);
+    result = this.applyRotation(result);
+    result = this.applyScale(result);
+    return result;
+  }
+
+  private applyTranslation(vector: Vector2D) {
+    const x = vector.x + this.position.x;
+    const y = vector.y + this.position.y;
+    return new Vector2D(x, y);
+  }
+
+  private applyRotation(vector: Vector2D) {
+    const rotationMatrix = this.calculateRotationMatrix();
+    const x = vector.x * rotationMatrix[0][0] + vector.y * rotationMatrix[1][0];
+    const y = vector.x * rotationMatrix[0][1] + vector.y * rotationMatrix[1][1];
+    return new Vector2D(x, y);
+  }
+
+  private calculateRotationMatrix(): [Vector2D, Vector2D] {
+    const cosine = Math.cos(this.rotation);
+    const sine = Math.sin(this.rotation);
+
+    return [
+      new Vector2D(cosine, sine),
+      new Vector2D(-sine, cosine),
+    ];
+  }
+
+  private applyScale(vector: Vector2D) {
+    const scaleMatrix = this.calculateScalingMatrix();
+    const x = vector.x * scaleMatrix[0][0] + vector.y * scaleMatrix[1][0];
+    const y = vector.x * scaleMatrix[0][1] + vector.y * scaleMatrix[1][1];
+    return new Vector2D(x, y);
+  }
+
+  private calculateScalingMatrix(): [Vector2D, Vector2D] {
+    return [
+      new Vector2D(this.scale.x, 0),
+      new Vector2D(0, this.scale.y),
+    ];
   }
 }
 
