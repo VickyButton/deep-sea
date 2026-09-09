94ca69c9a45be05910feebc0f9b6c8b8036536bc
vickybutton <vickythewebdev@gmail.com>
2026-09-09 14:12:56 -0500

    chore: Update test values

--- src/restructure/domain/Matrix2D.test.ts
+++ src/restructure/domain/Matrix2D.test.ts
@@ -5,29 +5,29 @@ import { describe, expect, it } from 'vitest';
 describe('Matrix2D', () => {
   it('should multiply a matrix', () => {
     const right = new Matrix2D([
-      new Vector2D(2, 4),
-      new Vector2D(8, 16),
+      new Vector2D(1, 2),
+      new Vector2D(3, 4),
     ]);
     const left = new Matrix2D([
-      new Vector2D(2, 2),
-      new Vector2D(2, 2),
+      new Vector2D(1, 2),
+      new Vector2D(3, 4),
     ]);
     const product = right.multiplyMatrix(left);
 
     expect(product).toEqual(new Matrix2D([
-      new Vector2D(12, 12),
-      new Vector2D(48, 48),
+      new Vector2D(7, 10),
+      new Vector2D(15, 22),
     ]));
   });
 
   it('should multiply a vector', () => {
     const matrix = new Matrix2D([
-      new Vector2D(2, 2),
-      new Vector2D(2, 2),
+      new Vector2D(1, 2),
+      new Vector2D(3, 4),
     ]);
-    const vector = new Vector2D(2, 4);
+    const vector = new Vector2D(1, 2);
     const result = matrix.multiplyVector(vector);
 
-    expect(result).toEqual(new Vector2D(12, 12));
+    expect(result).toEqual(new Vector2D(7, 10));
   });
 });
--- src/restructure/domain/Matrix3D.test.ts
+++ src/restructure/domain/Matrix3D.test.ts
@@ -5,33 +5,33 @@ import { describe, expect, it } from 'vitest';
 describe('Matrix3D', () => {
   it('should multiply a matrix', () => {
     const right = new Matrix3D([
-      new Vector3D(1, 4, 7),
-      new Vector3D(2, 5, 8),
-      new Vector3D(3, 6, 9),
+      new Vector3D(1, 2, 3),
+      new Vector3D(4, 5, 6),
+      new Vector3D(7, 8, 9),
     ]);
     const left = new Matrix3D([
-      new Vector3D(1, 4, 7),
-      new Vector3D(2, 5, 8),
-      new Vector3D(3, 6, 9),
+      new Vector3D(1, 2, 3),
+      new Vector3D(4, 5, 6),
+      new Vector3D(7, 8, 9),
     ]);
     const product = right.multiplyMatrix(left);
 
     expect(product).toEqual(new Matrix3D([
-      new Vector3D(30, 66, 102),
-      new Vector3D(36, 81, 126),
-      new Vector3D(42, 96, 150),
+      new Vector3D(30, 36, 42),
+      new Vector3D(66, 81, 96),
+      new Vector3D(102, 126, 150),
     ]));
   });
 
   it('should multiply a vector', () => {
     const matrix = new Matrix3D([
-      new Vector3D(1, 4, 7),
-      new Vector3D(2, 5, 8),
-      new Vector3D(3, 6, 9),
+      new Vector3D(1, 2, 3),
+      new Vector3D(4, 5, 6),
+      new Vector3D(7, 8, 9),
     ]);
     const vector = new Vector3D(1, 2, 3);
     const result = matrix.multiplyVector(vector);
 
-    expect(result).toEqual(new Vector3D(14, 32, 50));
+    expect(result).toEqual(new Vector3D(30, 36, 42));
   });
 });
