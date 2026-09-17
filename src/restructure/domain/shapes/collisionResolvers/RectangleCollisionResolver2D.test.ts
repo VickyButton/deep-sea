cd1c22fdf23ed4a9c0c9f6d85ae934a96d5902ee
vickybutton <vickythewebdev@gmail.com>
2026-09-17 05:59:33 -0500

    fix: Fix type issues

--- src/restructure/domain/shapes/collisionResolvers/RectangleCollisionResolver2D.test.ts
+++ src/restructure/domain/shapes/collisionResolvers/RectangleCollisionResolver2D.test.ts
@@ -1,7 +1,9 @@
+import type { BoundingBox2D } from '../shapes.types';
 import { PolygonRectanglePairCollisionResolver2D } from './PolygonRectanglePairCollisionResolver2D';
 import { RectangleCollisionResolver2D } from './RectangleCollisionResolver2D';
 import { RectanglePairCollisionResolver2D } from './RectanglePairCollisionResolver2D';
 import { Transform2D } from '../../Transform2D';
+import { Vector2D } from '../../Vector2D';
 import { PolygonShape2D } from '../PolygonShape2D';
 import { RectangleShape2D } from '../RectangleShape2D';
 import { Shape2D } from '../Shape2D';
@@ -48,12 +50,13 @@ describe('RectangleCollisionResolver2D', () => {
 });
 
 class UnknownShape extends Shape2D {
-  public boundingBox = {
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-  };
+  public boundingBox: BoundingBox2D = [
+    new Vector2D(),
+    new Vector2D(),
+    new Vector2D(),
+    new Vector2D(),
+  ];
   public isCollidingWith = vi.fn();
   public draw = vi.fn();
 }
+
