ef142e69614c6c1964ea2d2cd9c30d542ca72c3c
vickybutton <vickythewebdev@gmail.com>
2026-09-21 17:45:11 -0500

    feature: Export events

---
+++ src/restructure/events/index.ts
@@ -0,0 +1,12 @@
+/**
+ * This file defines event exports for the package.
+ */
+import { Event } from './Event';
+import { NewFrameEvent } from './NewFrameEvent';
+import { QueueDrawCommandEvent } from './QueueDrawCommandEvent';
+
+export {
+  Event,
+  NewFrameEvent,
+  QueueDrawCommandEvent,
+};
