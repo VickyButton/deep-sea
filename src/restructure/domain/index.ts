c9ea657848f7d44d74a58f2fd597a0e30924e5a1
vickybutton <vickythewebdev@gmail.com>
2026-09-22 02:35:27 -0500

    feature: Export vectors

---
+++ src/restructure/domain/index.ts
@@ -0,0 +1,10 @@
+/**
+ * This file defines domain exports for the package.
+ */
+import { Vector2D } from './Vector2D';
+import { Vector3D } from './Vector3D';
+
+export {
+  Vector2D,
+  Vector3D,
+};
