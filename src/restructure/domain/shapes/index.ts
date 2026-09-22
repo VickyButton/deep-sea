7ca2803381b94a35b55f53770d9cfa3bbbeea6dd
vickybutton <vickythewebdev@gmail.com>
2026-09-22 01:03:49 -0500

    feature: Export shapes

---
+++ src/restructure/domain/shapes/index.ts
@@ -0,0 +1,12 @@
+/**
+ * This file defines shape exports for the package.
+ */
+import { PolygonShape2D } from './PolygonShape2D';
+import { RectangleShape2D } from './RectangleShape2D';
+import { Shape2D } from './Shape2D';
+
+export {
+  PolygonShape2D,
+  RectangleShape2D,
+  Shape2D,
+};
