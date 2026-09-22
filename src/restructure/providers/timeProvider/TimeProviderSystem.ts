a37d6d2d045ae954448db8801d8f4ee93cd8a35e
vickybutton <vickythewebdev@gmail.com>
2026-09-22 02:14:18 -0500

    feature: Use performance.now instead of Date.now for time provider

--- src/restructure/providers/timeProvider/TimeProviderSystem.ts
+++ src/restructure/providers/timeProvider/TimeProviderSystem.ts
@@ -2,6 +2,6 @@ import type { TimeProvider } from '../timeProvider.types';
 
 export class TimeProviderSystem implements TimeProvider {
   public get now() {
-    return Date.now();
+    return performance.now();
   }
 }
