9da9b8c4ee103b189069d18736b3876245e43391
vickybutton <vickythewebdev@gmail.com>
2026-09-22 17:12:13 -0500

    chore: Condense JSDocs comments in frame loop types file

--- src/restructure/engine/frameLoop.types.ts
+++ src/restructure/engine/frameLoop.types.ts
@@ -1,18 +1,12 @@
-/**
- * Responsible for managing new frame timing and executing callback.
- */
+/** Responsible for managing frame timing and executing callback. */
 export interface FrameLoop {
   /**
    * Sets the frames per second that the frame loop should run at.
    * @param fps The frames per second.
    */
   setFramesPerSecond(fps: number): void;
-  /**
-   * Starts the frame loop.
-   */
+  /** Starts the frame loop. */
   start(): void;
-  /**
-   * Stops the frame loop.
-   */
+  /** Stops the frame loop. */
   stop(): void;
 }
