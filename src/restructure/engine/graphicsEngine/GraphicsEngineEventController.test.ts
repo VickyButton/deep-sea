a6151dff26d14eb147cf7099eac39d350ab21c96
vickybutton <vickythewebdev@gmail.com>
2026-09-23 02:05:12 -0500

    refactor: Refactor unit tests

--- src/restructure/engine/graphicsEngine/GraphicsEngineEventController.test.ts
+++ src/restructure/engine/graphicsEngine/GraphicsEngineEventController.test.ts
@@ -1,85 +1,64 @@
 import type { GraphicsEngineEvents } from '../graphicsEngine.types';
 import { GraphicsEngineEventController } from './GraphicsEngineEventController';
 import { Event } from '../../events/Event';
-import { describe, expect, it, vi } from 'vitest';
+import { afterAll, afterEach, beforeAll, describe, expect, it, vi } from 'vitest';
 
 describe('GraphicsEngineEventController', () => {
-  it('should map ClearCanvas event', () => {
-    const engine = new GraphicsEngine();
-    const events = createGraphicsEngineEvents();
-    const controller = new GraphicsEngineEventController(engine, events);
+  beforeAll(() => controller.startListening());
+  afterEach(() => vi.clearAllMocks());
+  afterAll(() => controller.stopListening());
 
-    controller.startListening();
+  it('should map ClearCanvas event', () => {
     events.ClearCanvas.emit();
 
     expect(engine.clearCanvas).toHaveBeenCalled();
   });
 
   it('should map ClearDrawCommandQueue event', () => {
-    const engine = new GraphicsEngine();
-    const events = createGraphicsEngineEvents();
-    const controller = new GraphicsEngineEventController(engine, events);
-
-    controller.startListening();
     events.ClearDrawCommandQueue.emit();
 
     expect(engine.clearDrawCommandQueue).toHaveBeenCalled();
   });
 
   it('should map DeleteCachedDrawCommand event', () => {
-    const engine = new GraphicsEngine();
-    const events = createGraphicsEngineEvents();
-    const controller = new GraphicsEngineEventController(engine, events);
-    const id = 'draw-command';
+    const data = 'draw-command';
 
-    controller.startListening();
-    events.DeleteCachedDrawCommand.emit(id);
+    events.DeleteCachedDrawCommand.emit(data);
 
-    expect(engine.deleteCachedDrawCommand).toHaveBeenCalledWith(id);
+    expect(engine.deleteCachedDrawCommand).toHaveBeenCalledWith(data);
   });
 
   it('should map ProcessDrawCommandQueue event', () => {
-    const engine = new GraphicsEngine();
-    const events = createGraphicsEngineEvents();
-    const controller = new GraphicsEngineEventController(engine, events);
-
-    controller.startListening();
     events.ProcessDrawCommandQueue.emit();
 
     expect(engine.processDrawCommandQueue).toHaveBeenCalled();
   });
 
   it('should map QueueDrawCommand event', () => {
-    const engine = new GraphicsEngine();
-    const events = createGraphicsEngineEvents();
-    const controller = new GraphicsEngineEventController(engine, events);
-    const drawCommand = {
+    const data = {
       id: 'draw-command',
       zIndex: 0,
       draw: vi.fn(),
     };
 
-    controller.startListening();
-    events.QueueDrawCommand.emit(drawCommand);
+    events.QueueDrawCommand.emit(data);
 
-    expect(engine.queueDrawCommand).toHaveBeenCalledWith(drawCommand);
+    expect(engine.queueDrawCommand).toHaveBeenCalledWith(data);
   });
 });
 
-const GraphicsEngine = vi.fn(class {
-  clearCanvas = vi.fn();
-  clearDrawCommandQueue = vi.fn();
-  deleteCachedDrawCommand = vi.fn();
-  processDrawCommandQueue = vi.fn();
-  queueDrawCommand = vi.fn();
-});
-
-function createGraphicsEngineEvents(): GraphicsEngineEvents {
-  return {
-    ClearCanvas: new Event(),
-    ClearDrawCommandQueue: new Event(),
-    DeleteCachedDrawCommand: new Event(),
-    ProcessDrawCommandQueue: new Event(),
-    QueueDrawCommand: new Event(),
-  };
+const engine = {
+  clearCanvas: vi.fn(),
+  clearDrawCommandQueue: vi.fn(),
+  deleteCachedDrawCommand: vi.fn(),
+  processDrawCommandQueue: vi.fn(),
+  queueDrawCommand: vi.fn(),
 };
+const events: GraphicsEngineEvents = {
+  ClearCanvas: new Event(),
+  ClearDrawCommandQueue: new Event(),
+  DeleteCachedDrawCommand: new Event(),
+  ProcessDrawCommandQueue: new Event(),
+  QueueDrawCommand: new Event(),
+};
+const controller = new GraphicsEngineEventController(engine, events);
