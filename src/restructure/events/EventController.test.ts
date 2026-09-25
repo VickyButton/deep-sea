5124d3d7f9624766da30284017de618ed1590c2f
vickybutton <vickythewebdev@gmail.com>
2026-09-25 15:56:01 -0500

    feature: Add unit test case for multiple listener callbacks

--- src/restructure/events/EventController.test.ts
+++ src/restructure/events/EventController.test.ts
@@ -3,91 +3,106 @@ import { EventController } from './EventController';
 import { describe, expect, it, vi } from 'vitest';
 
 describe('EventController', () => {
-  it('should listen for event if listener added before listening started', () => {
+  it('should listen for event if callback added before listening started', () => {
     const controller = new EventController();
     const event = new Event<void>();
-    const listener = vi.fn();
+    const callback = vi.fn();
 
-    controller.on(event, listener);
+    controller.on(event, callback);
     controller.startListening();
     event.emit();
 
-    expect(listener).toHaveBeenCalledOnce();
+    expect(callback).toHaveBeenCalledOnce();
   });
 
-  it('should listen for event if listener added after listening started', () => {
+  it('should accept multiple callbacks per listener', () => {
     const controller = new EventController();
     const event = new Event<void>();
-    const listener = vi.fn();
+    const callback1 = vi.fn();
+    const callback2 = vi.fn();
 
+    controller.on(event, callback1);
+    controller.on(event, callback2);
     controller.startListening();
-    controller.on(event, listener);
     event.emit();
 
-    expect(listener).toHaveBeenCalledOnce();
+    expect(callback1).toHaveBeenCalledOnce();
+    expect(callback2).toHaveBeenCalledOnce();
+  });
+
+  it('should listen for event if callback added after listening started', () => {
+    const controller = new EventController();
+    const event = new Event<void>();
+    const callback = vi.fn();
+
+    controller.startListening();
+    controller.on(event, callback);
+    event.emit();
+
+    expect(callback).toHaveBeenCalledOnce();
   });
 
   it('should not listen for event after listening stopped', () => {
     const controller = new EventController();
     const event = new Event<void>();
-    const listener = vi.fn();
+    const callback = vi.fn();
 
-    controller.on(event, listener);
+    controller.on(event, callback);
     controller.startListening();
     controller.stopListening();
     event.emit();
 
-    expect(listener).not.toHaveBeenCalled();
+    expect(callback).not.toHaveBeenCalled();
   });
 
-  it('should only map event to listener once', () => {
+  it('should listen for event after listening restarted', () => {
     const controller = new EventController();
     const event = new Event<void>();
-    const listener = vi.fn();
+    const callback = vi.fn();
 
-    controller.on(event, listener);
+    controller.on(event, callback);
     controller.startListening();
     controller.stopListening();
     controller.startListening();
     event.emit();
 
-    expect(listener).toHaveBeenCalledOnce();
+    expect(callback).toHaveBeenCalledOnce();
   });
 
   it('should not listen for event before listening started', () => {
     const controller = new EventController();
     const event = new Event<void>();
-    const listener = vi.fn();
+    const callback = vi.fn();
 
-    controller.on(event, listener);
+    controller.on(event, callback);
     event.emit();
 
-    expect(listener).not.toHaveBeenCalled();
+    expect(callback).not.toHaveBeenCalled();
   });
 
-  it('should not listen for event if listener is removed before starting', () => {
+  it('should not listen for event if callback is removed before starting', () => {
     const controller = new EventController();
     const event = new Event<void>();
-    const listener = vi.fn();
+    const callback = vi.fn();
 
-    controller.on(event, listener);
-    controller.remove(event, listener);
+    controller.on(event, callback);
+    controller.remove(event, callback);
     controller.startListening();
     event.emit();
 
-    expect(listener).not.toHaveBeenCalled();
+    expect(callback).not.toHaveBeenCalled();
   });
 
-  it('should not listen for event if listener is removed after starting', () => {
+  it('should not listen for event if callback is removed after starting', () => {
     const controller = new EventController();
     const event = new Event<void>();
-    const listener = vi.fn();
+    const callback = vi.fn();
 
     controller.startListening();
-    controller.on(event, listener);
-    controller.remove(event, listener);
+    controller.on(event, callback);
+    controller.remove(event, callback);
     event.emit();
 
-    expect(listener).not.toHaveBeenCalled();
+    expect(callback).not.toHaveBeenCalled();
   });
 });
